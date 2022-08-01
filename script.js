adff659810db5b6a3a58da21026e469eaa8d07bd
muskan2532150 <rg2532150@gmail.com>
2022-08-01 17:05:33 +0530

    Added localStorage

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-const addTitle = document.getElementById("Title")
-const addAuthor = document.getElementById("Author")
+const addTitle = document.getElementById("Title");
+const addAuthor = document.getElementById("Author");
+
 class books {
-  constructor(title, author){
-    this.title = title;
-    this.author = author;
-  }
+    constructor(title, author) {
+        this.title = title;
+        this.author = author;
+    }
 }
 
-class displayBooks {
-  static display() {
-    const storedBooks = [
-      {
-        title: 'Title 1',
-        author: 'Author 1'
-      }, 
-      {
-        title: 'Title 2',
-        author: 'Author 2'
-      }
-    ];
-    const books = storedBooks;
-    books.forEach(book => displayBooks.addBook(book))
-  }
+const storedBooks = [
+    //   {
+    //     title: 'Title 1',
+    //     author: 'Author 1'
+    //   }, 
+    //   {
+    //     title: 'Title 2',
+    //     author: 'Author 2'
+    //   }
+];
+function display() {
 
-  static addBook(book){
+    const books = storedBooks;
+    books.forEach(book => addBook(book));
+
+}
+
+function addBook(book) {
     const displayContainer = document.querySelector(".book-display-container")
     displayContainer.innerHTML += `
       <p class="book-title">${book.title}</p>
@@ -31,24 +33,36 @@ class displayBooks {
       <button class="remove-btn">remove</button>
       <hr>
     `
-  }
-
 
 }
 
-const addBook = document.querySelector('.book-form')
-addBook.addEventListener('submit', (e) =>{
-  e.preventDefault();
-  //creat a new objet and add to it
-  const title = addTitle.value;
-  const author = addAuthor.value;
+const addToBook = document.querySelector('.book-form')
+addToBook.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //creat a new objet and add to it
+    const title = addTitle.value;
+    const author = addAuthor.value;
 
-  const book = new books(title, author);
+    const book = new books(title, author);
+    storedBooks.push(book);
+    //add book to List
+    addBook(book);
+    localStorage.setItem('Book Details', JSON.stringify(storedBooks));
 
-  //add book to List
-  displayBooks.addBook(book)
 })
 
-//Load books
-document.addEventListener('DOMContentLoaded', displayBooks.display)
+function load() {
 
+    let getbooks = JSON.parse(localStorage.getItem('Book Details'));
+    console.log(getbooks);
+    if (getbooks != null) {
+        getbooks.forEach(bk => {
+            addBook(bk);
+            console.log(storedBooks);
+            storedBooks.push(bk);
+        })
+    }
+}
+
+//Load books
+document.addEventListener('DOMContentLoaded', load);
