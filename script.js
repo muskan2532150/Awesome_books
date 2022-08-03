4558e0440a08dcafb93cdfc7988cd7d1056411fb
muskan2532150 <rg2532150@gmail.com>
2022-08-03 19:26:08 +0530

    Refactoring and added time

--- script.js
+++ script.js
@@ -1,102 +1,92 @@
 import Books from './books.js';
+import LocalStore from './LocalStorage.js';
 
-const addTitle = document.getElementById('Title');
-const addAuthor = document.getElementById('Author');
+const navLink = document.querySelectorAll('.nav');
+const contact = document.querySelector('.contact');
+const bookSection = document.querySelector('.display-area');
+const addBook = document.querySelector('.add-book');
 
-function getlocalStorage() {
-  return JSON.parse(localStorage.getItem('Book Details'));
-}
-
-function setlocalStorage(store) {
-  localStorage.setItem('Book Details', JSON.stringify(store));
-}
-
-class BookDetails {
-  constructor(book = []) {
-    this.storedBooks = book;
+class Display {
+  static displayBooks() {
+    const books = LocalStore.getBooks();
+    books.forEach((book) => Display.addBook(book));
   }
 
-  addBook(book) {
+  static addBook(book) {
     const displayContainer = document.querySelector('.book-display-container');
     const bookContainer = document.createElement('div');
     bookContainer.className = 'book-container';
-    const id = this.storedBooks.length + 1;
-    bookContainer.setAttribute('id', id);
     bookContainer.innerHTML = `
         <p class="book-title">"${book.title}" by ${book.author}</p>
-        <button class="remove-btn">Remove</button>
+        <button id= "${book.id}"class="remove-btn">Remove</button>
     `;
     displayContainer.appendChild(bookContainer);
   }
 
-  removeBook(id) {
-    for (let i = 0; i < this.storedBooks.length; i += 1) {
-      if (this.storedBooks[i].id === parseInt(id, 10)) {
-        this.storedBooks.splice(i, 1);
-      }
+  static clearForm() {
+    document.getElementById('Title').value = '';
+    document.getElementById('Author').value = '';
+  }
+
+  static deleteBook(element) {
+    if (element.classList.contains('remove-btn')) {
+      element.parentElement.remove();
     }
-    setlocalStorage(this.storedBooks);
   }
 }
-const store = new BookDetails();
 
+document.addEventListener('DOMContentLoaded', Display.displayBooks);
+// Add a book from Event Listener
 const addToBook = document.querySelector('.book-form');
-addToBook.addEventListener('submit', () => {
-  // creat a new objet and add to it
-  const title = addTitle.value;
-  const author = addAuthor.value;
-  const id = store.storedBooks.length + 1;
-  const book = new Books(title, author, id);
-  //  Push the book obj to array
-  store.storedBooks.push(book);
-  // add book to List
-  store.addBook(book);
-  setlocalStorage(store.storedBooks);
+addToBook.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // Grab the Values
+  const id = LocalStore.idGenerator();
+  const title = document.getElementById('Title').value;
+  const author = document.getElementById('Author').value;
+  // Instantiate a new Book class Object
+  const book = new Books(id, title, author);
+  // add to Display UI
+  Display.addBook(book);
+  // Add to LocalStore
+  LocalStore.addBook(book);
+  // Clear from Area
+  Display.clearForm();
   bookSection.classList.add('active');
   contact.classList.remove('active');
   addBook.classList.remove('active');
 });
 
-const getbooks = getlocalStorage();
-if (getbooks != null) {
-  getbooks.forEach((bk) => {
-    store.addBook(bk);
-    store.storedBooks.push(bk);
-  });
-}
-
-const removeBooks = document.querySelectorAll('.remove-btn');
-removeBooks.forEach((book) => {
-  book.addEventListener('click', () => {
-    store.removeBook(book.parentElement.id);
-    book.parentElement.remove();
-  });
+const displayContainer = document.querySelector('.book-display-container');
+displayContainer.addEventListener('click', (e) => {
+  // remove from UI
+  Display.deleteBook(e.target);
+  // remove from LocalStore
+  LocalStore.removeBook(e.target.id);
+  // remove from LocalStore
 });
 
-
-// --
-const navLink = document.querySelectorAll('.nav');
-const contact = document.querySelector(".contact")
-const bookSection = document.querySelector(".display-area");
-const addBook = document.querySelector(".add-book");
-
-console.log(navLink)
-navLink.forEach((link,index) => {
-  link.addEventListener('click', ()=>{
-    // console.log(link)
-    if(index===2){
+navLink.forEach((link, index) => {
+  link.addEventListener('click', () => {
+    if (index === 2) {
       contact.classList.add('active');
       addBook.classList.remove('active');
       bookSection.classList.remove('active');
-    } else if(index===0){
-    bookSection.classList.add('active');
-    contact.classList.remove('active');
-    addBook.classList.remove('active');
-    }
-    else {
+    } else if (index === 0) {
+      bookSection.classList.add('active');
+      contact.classList.remove('active');
+      addBook.classList.remove('active');
+    } else {
       addBook.classList.add('active');
       bookSection.classList.remove('active');
       contact.classList.remove('active');
     }
-  })
+  });
 });
+
+const Time = document.querySelector('.time');
+const date = new Date();
+const format = {
+  month: 'long', weekday: 'long', day: 'numeric', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit',
+};
+Time.textContent = date.toLocaleTimeString('en-us', format);
